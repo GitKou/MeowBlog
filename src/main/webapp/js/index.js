295b67942cae43813b6884354052ea13f7f89fbb
gaomiaomiao <hzgaomiaomiao@corp.netease.com>
2016-07-24 17:48:51 +0800

    misc:新增首页，原blog页面换header

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -6,19 +6,17 @@ $(window).scroll(function() {
     });
 
     $('.logo').css({
-        'transform': 'translate(0px, ' + wScroll / 1.3 + '%)'
+        'transform': 'translate(-50%, ' + wScroll / 1.3 + '%)'
     });
 
     $('.pen').css({
         'transform': 'translate(0px, ' + wScroll / 2 + '%)'
     });
 
-    // $('.pen').css({
-    //     'transform': 'translate('+ wScroll / 2 +'%, 0)'
-    // });
     $('.tree').css({
-        'transform': 'translate(0px, ' + wScroll / 8 + '%)'
+        'transform': 'translate(0px, -' + wScroll / 8 + '%)'
     });
+    /*顺序显示相册图片*/
     if (wScroll > $('.albums').offset().top - $(window).height() / 1.5) {
         $('.albums figure').each(function(i) {
             setTimeout(function() {
@@ -26,6 +24,7 @@ $(window).scroll(function() {
             }, 150 * (i + 1));
         });
     }
+    /*滚动改变透明度*/
     if (wScroll > $('.large-window').offset().top - $(window).height() / 2) {
         var opacity = wScroll / $('.large-window').offset().top;
         $('.window-tint').css({
@@ -33,13 +32,14 @@ $(window).scroll(function() {
         });
 
     }
+    /*两边分开*/
     if (wScroll > $('.blog-posts').offset().top - $(window).height()) {
         var offset = Math.min(0, wScroll - $('.blog-posts').offset().top + 350);
         $('.post-1').css({
             'transform': 'translate(' + offset + 'px ,' + Math.abs(offset * 0.5) + 'px)'
         });
         $('.post-3').css({
-            'transform': 'translate(' + Math.abs(offset) + 'px ,' + Math.abs(offset * 0.2) + 'px)'
+            'transform': 'translate(' + Math.abs(offset) + 'px ,' + Math.abs(offset * 0.5) + 'px)'
         });
     }
 
